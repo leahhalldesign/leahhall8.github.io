5582842e5f8780cb0b14e7e186c7e4e04a52f736
leahhall8 <hal16034@byui.edu>
2020-02-20 11:07:43 -0700

    pankcakefriday

--- lesson6/scripts/main.js
+++ lesson6/scripts/main.js
@@ -49,10 +49,10 @@ document.querySelector('#current-date').textContent = fullDate;
 
 
 /* PANCAKE */
-const currentDate = newDate();
+const pancake = new Date();
 const aside = document.querySelector('aside');
 
-if (currentDate.getDay() === 5) {
+if (pancake.getDay() === 5) {
     aside.style.display = 'block';
 }
     else {
@@ -66,12 +66,13 @@ s = 10;
 
 if (t<=50 && s >= 3) {
     f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
-    document.querySelector('#windChill').innerHTML = f.toFixed(0) + '&deg';
-}else {
-    f = 'N/A';
-    document.querySelector('#windChill').innerHTML = f;
+    document.querySelector('#wind-chill').innerHTML = f.toFixed(0) + '&deg';
 }
+    else {
+        f = 'N/A';
+        document.querySelector('#wind-chill').innerHTML = f;
+    }
 
 document.querySelector('#current-temp').innerHTML = t + '&deg';
-document.querySelector('#windSpeed').innerHTML = s + 'MPH';
+document.querySelector('#wind-speed').innerHTML = s + 'MPH';
 
