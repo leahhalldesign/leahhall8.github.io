aad6cf1ffffb0ad7c7d7c8bacd9e206bd6fd37aa
leahhall8 <hal16034@byui.edu>
2020-02-20 10:29:37 -0700

    webfontloader

--- lesson6/scripts/main.js
+++
@@ -1,7 +0,0 @@
-var WebFont = require('webfontloader');
-
-WebFont.load({
-  google: {
-    families: ['Bebas Neue', 'Montserrat:200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800']
-  }
-});
