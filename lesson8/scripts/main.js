ecf62693fedfd2567f9564b63cb9586a6580eb26
leahhall8 <hal16034@byui.edu>
2020-02-26 11:29:23 -0700

    javaupdate

--- lesson8/scripts/main.js
+++ lesson8/scripts/main.js
@@ -46,33 +46,3 @@ fullDate += ' ' + year;
 
 /* full date */
 document.querySelector('#current-date').textContent = fullDate;
-
-
-/* PANCAKE */
-const pancake = new Date();
-const aside = document.querySelector('aside');
-
-if (pancake.getDay() === 5) {
-    aside.style.display = 'block';
-}
-    else {
-        aside.style.display = 'none';
-    }
-
-/* WEATHER SUMMARY */
-let f,t,s;
-t = 50;
-s = 10;
-
-if (t<=50 && s >= 3) {
-    f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
-    document.querySelector('#wind-chill').innerHTML = f.toFixed(0) + '&deg';
-}
-    else {
-        f = 'N/A';
-        document.querySelector('#wind-chill').innerHTML = f;
-    }
-
-document.querySelector('#current-temp').innerHTML = t + '&deg';
-document.querySelector('#wind-speed').innerHTML = s + 'mph';
-
