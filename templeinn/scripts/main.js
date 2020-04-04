31a56a14434c51506fad0bf033f88404a57e4398
leahhall8 <hal16034@byui.edu>
2020-04-03 22:12:52 -0600

    json

---
+++ templeinn/scripts/main.js
@@ -0,0 +1,16 @@
+fetch('json/temples.json')
+    .then(response => response.json())
+    .then(
+        response => {
+            document.querySelector('#temple1-name').textContent = 
+                response[0].TempleName;
+
+            response[0].Services.forEach(
+                service => {
+                    document.querySelector('#services1').innerHTML +=
+                        '<li>${ service }</li>'
+                }
+            )
+            document.querySelector('#image1').setAttribute('src', response[0].Image);
+        }
+    )
